bbb29de1787e40944f6774ad7a1e25ba45cc3dac
Antech15 <shakycrab@gmail.com>
2023-10-10 20:06:26 -0700

    Signup, Login Authentication, and Logout

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask,render_template,request, redirect, url_for, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from flask_sqlalchemy import SQLAlchemy #for the database
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 import jwt
 
 import sys
@@ -31,13 +35,51 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'mewhenthe'
 
+#Bcrypt instance
+bcrypt = Bcrypt(app)
+
 #Creates the database instance
 db = SQLAlchemy(app)
+app.app_context().push()
+
+
+login_manager = LoginManager() 
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+#reload user from stored id in the session
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)#Identity column for user
     username = db.Column(db.String(20), nullable = False, unique=True)#User's name (20 char max, can't be empty, must be unique)
     password = db.Column(db.String(80), nullable = False)#Password (80 char max, can't be empty)
+
+#Signup form
+class SignupForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Sign Up")
+    
+#If username exists, give an error
+def validate_username(self, username):
+    existing_user_username = User.query.filter_by(
+        username=username.data).first()
+    if existing_user_username:
+        raise ValidationError(
+            "That username already exists. Please choose a different one.")
+    
+#Login form
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
 
 FlaskJSON(app)
 
@@ -50,8 +92,8 @@ def init_new_env():
     if 'hb' not in g:
         g.hb = get_head_band_sensor_object()
 
-    g.secrets = get_secrets()
-    g.sms_client = get_sms_client()
+    #g.secrets = get_secrets()
+    #g.sms_client = get_sms_client()
 
 #This gets executed by default by the browser if no page is specified
 #So.. we redirect to the endpoint we want to load the base page
@@ -60,14 +102,44 @@ def index():
     return render_template('index.html')
 
 #This is the login page
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+#Once the use is logged in, they go to the logged in dashboard
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 #This is the Signup page
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = SignupForm()
+
+    if form.validate_on_submit():
+        #creates hashed password to encrypt it
+        hashed_password= bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data,password=hashed_password)
+        #new user is created
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('signup.html', form = form)
 
 @app.route('/video')
 def video():
@@ -121,5 +193,5 @@ def exec_proc(proc_name):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
 
