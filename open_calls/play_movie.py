a8c7c53739d04504f75f31283463e269e1ac8493
Andres Hinojosa <shakycrab@gmail.com>
2023-10-23 20:08:02 -0700

    play_movie

--- open_calls/play_movie.py
+++ open_calls/play_movie.py
@@ -3,17 +3,26 @@ from tools.logging import logger
 from neurosdk.cmn_types import * 
 from flask import request
 from tools.eeg import on_brain_bit_signal_data_received, Sensor, test, change_user_and_vid
+import os
 
 def handle_request():
     #if g.hb == None:
     #    return ["Data Flowing"]
 
+    #Gets the video id and user id from the request
     video_id = request.form.get('video_id')
     user_id = request.form.get('user_id')
-    change_user_and_vid(user_id + "_" + video_id + ".pkl")
 
-    print("video id is " + video_id)
-    print("user id is " + user_id)
+    #changes the filename to the user_id + video_id
+    name = user_id + "_" + video_id + ".pkl"
+    change_user_and_vid(name)
+
+    #if file exists, delete it
+    if os.path.exists(name):
+        os.remove(name)
+
+    #test data
     test()
+
     g.hb.exec_command(SensorCommand.CommandStartSignal)
     return ["Data Flowing"]
